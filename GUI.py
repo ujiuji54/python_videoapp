c147093e3b144030326ed859c639316e1c3bacc3
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-03-03 20:50:08 +0900

    kivyチュートリアル

---
+++ GUI.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.graphics import Color, Ellipse
+
+class MyPaintWidget(Widget):
+    
+    def on_touch_down(self, touch):
+        with self.canvas:
+            Color(1, 1, 0)
+            d = 30.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+
+class MyPaintApp(App):
+
+    def build(self):
+        return MyPaintWidget()
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
 from play import *
+from GUI import *
 
 if __name__ == '__main__':
     video = cv2.VideoCapture(u"video.mp4")
-    play(video)
+    #play(video)
+    MyPaintApp().run()
+
